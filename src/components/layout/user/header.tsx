8f94f40abf0566c25992818665433630dd196f4c
kientt <trankien3002721@gamil.com>
2024-01-29 09:09:33 +0700

    first commit

---
+++ src/components/layout/user/header.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const HeaderU = () => {
+  return (
+    <header className="bg-[#D70018] h-[64px] px-20 items-center">
+      <div className="container mx-auto flex items-center gap-4">
+        <img className="w-[50px] mt-3" src="/logo.png/" alt="" />
+
+        <div className="relative w-full mt-3">
+          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <svg
+              aria-hidden="true"
+              className="w-5 h-5 text-gray-500 dark:text-gray-400"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              ></path>
+            </svg>
+          </div>
+          <input
+            type="search"
+            id="default-search"
+            className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Search ..."
+            required
+          />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default HeaderU;
